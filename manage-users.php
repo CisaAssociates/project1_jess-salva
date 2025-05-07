74ea29a9969aaf642ccdc730fce2107da11f13da
Joshua Alboleras <albolersajohualuis@gmail.com>
2025-05-07 22:57:42 +0800

    fasdfa

--- manage-users.php
+++ manage-users.php
@@ -102,7 +102,7 @@ $result_users = mysqli_stmt_get_result($stmt_users);
                         </a>
                     </li> -->
                     <li>
-                        <a href="./attendance-dashboard.php" class="nav-link ">
+                        <a href="./admin-gatepass-logs.php" class="nav-link ">
                             <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg> <span>Filtered Logs</span>
                         </a>
                     </li>
