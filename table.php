f7ea7e86d4897d0ee27dfc2d53f7c2c4a4ed7b48
Joshua Alboleras <albolersajohualuis@gmail.com>
2025-05-08 09:03:16 +0800

    sdfa

--- table.php
+++ table.php
@@ -8,7 +8,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM faceencodings INNER JOIN users on faceencodings.user_id = users.user_id";
+$sql = "SELECT * FROM faceencodings INNER JOIN users on faceencodings.user_id = users.user_id INNER JOIN ON rfidcards ON users.user_id = rfidcards.user_id";
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
